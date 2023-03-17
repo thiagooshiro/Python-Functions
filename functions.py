a7f3fd2edcddb6ceb49e51a611ed2efe74a30d52
Thiago Oshiro <oshirothiago@gmail.com>
2023-03-17 19:59:40 -0300

    first commit

---
+++ functions.py
@@ -0,0 +1,23 @@
+
+somar = 2 + 3
+
+subtrair = 5 - 4
+
+multiplicar = 4 * 3
+
+dividir = 4 // 2
+
+
+def soma(numA, numB):
+    resultado = numA + numB
+    return resultado
+
+soma(2, 4)
+
+print('1.adição')
+print('2.subtração')
+print('3.multiplicação')
+print('4.divisão')
+
+
+first_number = input('Digite o primeiro valor')
---
+++ javascriptexample.js
@@ -0,0 +1,14 @@
+let x = 4
+
+if (x > 2) {
+  console.log(true)
+}
+
+let sun = true
+let weekend = true
+
+if (sun == true && weekend == true) {
+  console.log('PRAIA!')
+} else {
+  console.log('=(')
+}
