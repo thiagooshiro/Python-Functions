edfe62de79f3ba6a45b316535ae20e1c3a0c2bce
Giorgio Almeida <118644686+Giorgioeab@users.noreply.github.com>
2023-03-18 21:01:33 -0300

    Update friendlist.py
    resolução

--- friendlist.py
+++ friendlist.py
@@ -1,3 +1,6 @@
+ # Desafio é imprimir:
+# nome dos aniversariantes seguido do presente favorito;
+
 amigos_aniversariantes = ['Bruna', 'Letícia']
 
 todos_amigos = {
@@ -8,12 +11,8 @@ todos_amigos = {
 }
 
 
-
 def lista_amigos(lista, dictionary):
-    
-    # Desafio é imprimir:
-# nome dos aniversariantes seguido do presente favorito;
-
+  
 aniversariantes_do_mes = ['Bruna', 'Letícia']
 
 todos_amigos = {
@@ -23,23 +22,13 @@ todos_amigos = {
     "Letícia": {"aniversário": '02/03', "presente": 'Plantinhas'},
 }
 
-#Sem parmetros
-'''def amigos():
-    for item in todos_amigos:
-        for nome in aniversariantes_do_mes:
-            if item == nome:
-                print(nome, todos_amigos[item]["aniversário"], todos_amigos[item]["presente"])
 
-amigos()'''
-
-#conforme o exercicio
 def lista_amigos(lista, dicionario):
     for nome in lista:
         for item in dicionario:
             if nome == item:
                 print(nome, dicionario[item]["aniversário"], dicionario[item]["presente"])
 
-lista_amigos(aniversariantes_do_mes, todos_amigos)
 
 lista_amigos(amigos_aniversariantes, todos_amigos)
 
